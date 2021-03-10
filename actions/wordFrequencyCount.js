46a6a88d535f49d5129d96c3b246a264b6327340
DESKTOP-P4PKBUR\damin <zhangxue@neurongenius.com>
2021-03-10 11:22:38 +0800

    add data

--- actions/wordFrequencyCount.js
+++ actions/wordFrequencyCount.js
@@ -4,28 +4,52 @@
 const nodejieba = require("nodejieba");
 const data_5188 = require('../result/5188-step1-words.json');
 const {writeJSON} = require("../spider/utils");
+const Excel = require('exceljs');
 
-// 分词
 const allData = [];
-for (let i = 0, length = data_5188.length; i < length; i++) {
-    let wordSplit = nodejieba.cut(data_5188[i], true);
-    allData.push(...wordSplit);
+
+async function readXLSX(){
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile('../result/导出2021-3-8.xlsx');
+    const worksheet = workbook.getWorksheet(1);
+    worksheet.getColumn(1).eachCell(function(cell, rowNumber) {
+        let wordSplit = nodejieba.cut(cell.text, true);
+        allData.push(...wordSplit);
+    });
+}
+
+
+function read5188File(){
+    for (let i = 0, length = data_5188.length; i < length; i++) {
+        let wordSplit = nodejieba.cut(data_5188[i], true);
+        allData.push(...wordSplit);
+    }
 }
 
 // 统计和排序
-const result = [];
-allData.forEach((keyword) => {
-    if(keyword.length > 1){
-        const target = result.find(i => i.key === keyword);
-        if(target){
-            target.count += 1;
-        } else {
-            result.push({ key: keyword, count: 0 })
+function sort(){
+    const result = [];
+    allData.forEach((keyword) => {
+        if(keyword.length > 1){
+            const target = result.find(i => i.key === keyword);
+            if(target){
+                target.count += 1;
+            } else {
+                result.push({ key: keyword, count: 0 })
+            }
         }
-    }
-});
+    });
+    result.sort((a, b) => a.count-b.count > 0 ? -1 : 1);
+    return result;
+}
 
-result.sort((a, b) => a.count-b.count > 0 ? -1 : 1);
 
+async function start(){
+    await readXLSX();
+    read5188File();
+    const result = sort();
 
-writeJSON(result, '../result/word-frequency.json')
+    await writeJSON(result, '../result/word-frequency.json')
+}
+
+start();
